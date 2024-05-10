3204c1d664d09640dd8d7e40d566436c5cd06289
SeanMero <MeroFromVero@gmail.com>
2024-05-10 16:50:27 +0800

    Add recursive Fibonacci function

--- recursion.rb
+++ recursion.rb
@@ -1,5 +1,7 @@
-# create an array of the first n Fibonacci numbers.
-# N must be a positive integer.
+require 'pry-byebug'
+
+# create an array of the first n Fibonacci numbers iteratively.
+# n must be a positive integer.
 def fibs(n)
   arr = []
   n.times do |i|
@@ -7,4 +9,14 @@ def fibs(n)
     arr.push(arr[i-1] + arr[i-2]) if i > 1
   end
   arr
+end
+
+# produce the nth Fibonacci number recursively.
+# n must be a positive integer.
+def fibs_rec(n)
+  if n == 0 || n == 1
+    n
+  else
+    fibs_rec(n-1) + fibs_rec(n-2)
+  end
 end
